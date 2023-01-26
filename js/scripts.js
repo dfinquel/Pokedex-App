2c40cdc1f801146c412a4cff32eab4387330eae9
dfinquel <119966119+dfinquel@users.noreply.github.com>
2023-01-26 21:54:47 +0100

    Append button as child of listItem

--- js/scripts.js
+++ js/scripts.js
@@ -191,10 +191,13 @@ let pokemonRepository = (function () {
 pokemonRepository.getAll().forEach(function(pokemon) {
 
     let list = document.querySelector('.pokemon-list');
+
     let listItem = document.createElement('li')
+
     let button = document.createElement('button')
     button.innerText = pokemon.name;
     button.classList.add('pokemon-item');
+    button.appendChild(listItem);
     
 
     /*Old loop block with conditional. Commented out and replaced by code above.
