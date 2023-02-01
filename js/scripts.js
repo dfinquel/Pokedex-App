5c5e8ac77fd89c42de7049aaa177c02f0af94ba1
dfinquel <119966119+dfinquel@users.noreply.github.com>
2023-02-01 12:42:12 +0100

    Add group-list-item class to li element

--- js/scripts.js
+++ js/scripts.js
@@ -30,6 +30,7 @@ let pokemonRepository = (function () {
         let list = document.querySelector('.pokemon-list');
 
         let listItem = document.createElement('li');
+        listItem.classList.add('group-list-item');
 
         let button = document.createElement('button');
         button.innerText = pokemon.name;
