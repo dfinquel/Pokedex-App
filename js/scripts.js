cbb1916b00609b54fca3d714f61ddf3b86ce0252
dfinquel <119966119+dfinquel@users.noreply.github.com>
2023-02-03 16:25:01 +0100

    Correct showModal function to include types

--- js/scripts.js
+++ js/scripts.js
@@ -105,8 +105,16 @@ let pokemonRepository = (function () {
         let pokemonHeight = document.createElement('p');
             pokemonHeight.innerText =('Height: ' + (pokemon.height * 10) + 'cm.' +'\n');
 
+        /*Below, added a pokemonTypes variable, which creates a <p> element. 
+        Then, the constant "types" is set to retrieve the type names from the types object, and join them with ", ". 
+        Finally, the inner text of the variable is set to display "Types: " followed by the types const.*/
         let pokemonTypes = document.createElement('p');
-            pokemonTypes.innerText =('Types: ' + pokemon.types +'\n');
+        const types = pokemon.types.map((item) => item.type.name).join(", ");
+        pokemonTypes.innerText = 'Types: ' + types;
+
+        /*Below, set innerHTML of Title and Body to be strings.*/
+        modalTitle.innerHTML = "";
+        modalBody.innerHTML = "";
 
         modalTitle.append(pokemon.name);
         modalBody.append(pokemonSprite);
