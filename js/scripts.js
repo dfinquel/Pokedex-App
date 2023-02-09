59a79329686f667803402185b5079238d47b7489
dfinquel <119966119+dfinquel@users.noreply.github.com>
2023-02-09 12:23:22 +0100

    Change imageUrl in loadDetails function

--- js/scripts.js
+++ js/scripts.js
@@ -79,7 +79,7 @@ let pokemonRepository = (function () {
             return response.json();
         }).then(function (details) {
             /*Below, added the details to the item.*/
-            item.imageUrl = details.sprites.front_default;
+            item.imageUrl = details.sprites.other["official-artwork"].front_default;
             item.height = details.height;
             item.types = details.types;
         }).catch(function (e) {
@@ -87,39 +87,18 @@ let pokemonRepository = (function () {
         });
       }
 
+    //const movies = await axios("http://david.com/getmovies");
+    // or
+    /*   axios("http://david.com/getmovies")
+    .then(movies => { // callback fn
+        //logic here
+    })
+    .catch(e => console.log(e)) */
+
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function () {
             showModal(pokemon);
         });
-    }
-
-    function showModal(pokemon) {
-
-        let modalTitle = document.querySelector('.modal-title');
-        let modalBody = document.querySelector('.modal-body');
-
-        let pokemonSprite = document.createElement('img');
-            pokemonSprite.classList.add ('sprite');
-            pokemonSprite.src = pokemon.imageUrl;
-        
-        let pokemonHeight = document.createElement('p');
-            pokemonHeight.innerText =('Height: ' + (pokemon.height * 10) + 'cm.' +'\n');
-
-        /*Below, added a pokemonTypes variable, which creates a <p> element. 
-        Then, the constant "types" is set to retrieve the type names from the types object, and join them with ", ". 
-        Finally, the inner text of the variable is set to display "Types: " followed by the types const.*/
-        let pokemonTypes = document.createElement('p');
-        const types = pokemon.types.map((item) => item.type.name).join(", ");
-        pokemonTypes.innerText = 'Types: ' + types;
-
-        /*Below, set innerHTML of Title and Body to be strings.*/
-        modalTitle.innerHTML = "";
-        modalBody.innerHTML = "";
-
-        modalTitle.append(pokemon.name);
-        modalBody.append(pokemonSprite);
-        modalBody.append(pokemonHeight);
-        modalBody.append(pokemonTypes);
     }
 
     return {
